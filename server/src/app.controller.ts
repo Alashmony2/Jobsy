11105b3906c493cc93ae7135db6212b058d271b4
Alashmony2 <ahmedSamyAlashmony1@gmail.com>
2026-02-03 20:54:32 +0200

    create a auth module and link router

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,5 +1,8 @@
 import { type Express } from "express";
+import authRouter from "./module/auth/auth.controller";
 export function bootstrap(app: Express, express: any) {
+  //auth 
+  app.use("/auth", authRouter);
   app.use("/{*dummy}", (req, res, next) => {
     return res.status(404).json({ message: "Invalid router", success: false });
   });
